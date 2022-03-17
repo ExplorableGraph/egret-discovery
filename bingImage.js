d86794dfae9f4b60e8ae79eed1968494132dfe9e
Jan Miksovsky <jan@miksovsky.com>
2022-03-16 17:38:06 -0700

    Store Bing Search API key in separate env.js file outside of source control

--- bingImage.js
+++ bingImage.js
@@ -1,13 +1,12 @@
 import fetch from "node-fetch";
-
-const apiKey = "f30b88c434004a1aa025382b225cf7d1";
+import { BING_SEARCH_API_KEY } from "./env.js";
 
 export default async function bing(query) {
   const response = await fetch(
     `https://api.bing.microsoft.com/v7.0/images/search?q=${query}&count=1&aspect=square&imageType=photo`,
     {
       headers: {
-        "Ocp-Apim-Subscription-Key": apiKey,
+        "Ocp-Apim-Subscription-Key": BING_SEARCH_API_KEY,
       },
     }
   );
