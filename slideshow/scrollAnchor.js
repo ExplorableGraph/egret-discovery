22c2ccf8d3d618c31a60b6cc138ddcb7c018dee2
Jan Miksovsky <jan@miksovsky.com>
2022-03-17 16:15:14 -0700

    Slides

---
+++ slideshow/scrollAnchor.js
@@ -0,0 +1,38 @@
+function getCurrentSlideNumber() {
+  const scrollTop = document.body.scrollTop;
+  const slides = [...document.body.children];
+  for (let i = 0; i < slides.length; i++) {
+    if (slides[i].offsetTop >= scrollTop) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+function updateHashFromSlide() {
+  const slideNumber = getCurrentSlideNumber();
+  const newHash = `#${slideNumber}`;
+  if (location.hash !== newHash) {
+    location.hash = newHash;
+  }
+}
+
+function updateSlideFromHash() {
+  const currentSlideNumber = getCurrentSlideNumber();
+  const hash = location.hash;
+  const hashSlideNumber = hash ? parseInt(hash.substring(1)) : -1;
+  if (hashSlideNumber !== currentSlideNumber) {
+    document.body.children[hashSlideNumber].scrollIntoView();
+  }
+}
+
+window.addEventListener("hashchange", () => {
+  updateSlideFromHash();
+});
+
+window.addEventListener("load", () => {
+  document.body.addEventListener("scroll", () => {
+    setTimeout(updateHashFromSlide);
+  });
+  updateSlideFromHash();
+});
