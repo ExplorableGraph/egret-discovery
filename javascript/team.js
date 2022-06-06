d8756758517385fa8835315007e76dae291bdce6
Jan Miksovsky <jan@miksovsky.com>
2022-06-06 09:59:39 -0700

    JavaScript version

---
+++ javascript/team.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: "Alice",
+    avatar: "golden retriever",
+  },
+  {
+    name: "Bob",
+    avatar: "palm trees",
+  },
+  {
+    name: "Carol",
+    avatar: "tropicbird flying",
+  },
+];
